cb75efb76dedff54028f8a58684a4a2e06b62705
mr-ramzan01 <ramzankhanmansoori0786@gmail.com>
2022-11-10 16:25:21 +0530

    skeleton done

--- my_portfolio/src/Components/GithubStats.jsx
+++ my_portfolio/src/Components/GithubStats.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
+import '../css/GithubStats.modules.css'
 
 export const GithubStats = () => {
   return (
-    <div>
-        <picture>
-            <source 
-            srcset="https://github-readme-stats.vercel.app/api?username=anuraghazra&show_icons=true&theme=dark"
-            media="(prefers-color-scheme: dark)"
-            />
-            <source
-            srcset="https://github-readme-stats.vercel.app/api?username=anuraghazra&show_icons=true"
-            media="(prefers-color-scheme: light), (prefers-color-scheme: no-preference)"
-            />
-            <img src="https://github-readme-stats.vercel.app/api?username=anuraghazra&show_icons=true" />
-        </picture>
+    <div className='githubStats'>
+        <img src="https://github-readme-stats.vercel.app/api?username=mr-ramzan01&show_icons=true&locale=en&border_radius=10" alt="" />
+        <img src="	https://github-readme-streak-stats.herokuapp.com/?user=mr-ramzan01&border_radius=10" alt="" />
+        <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=mr-ramzan01&layout=compact&border_radius=10" alt="" />
     </div>
   )
 }
