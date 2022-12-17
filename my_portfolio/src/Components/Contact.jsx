a86b2fbb867311611aab8bde57b59ff7faafc244
mr-ramzan01 <ramzankhanmansoori0786@gmail.com>
2022-12-17 13:11:26 +0530

    responsiveness done

--- my_portfolio/src/Components/Contact.jsx
+++ my_portfolio/src/Components/Contact.jsx
@@ -32,50 +32,50 @@ export const Contact = () => {
             <div>
               <div className='contact_details'>
                 <div className='my_details'>
-                  <div data-aos="fade-right" data-aos-delay='2500' data-aos-duration='1000'>
+                  <div data-aos="fade-right" data-aos-delay='2000' data-aos-duration='1000'>
                     <h2 style={{color: "#000000"}}>Get in touch</h2>
                   </div>
                   <div>
-                    <div data-aos="fade-right" data-aos-delay='1000' data-aos-duration='1000' className='contact_icon_div'>
+                    <div data-aos="fade-right" data-aos-delay='500' data-aos-duration='1000' className='contact_icon_div'>
                       <i style={theme?{color: '#2f4356'}:{color: '#6a6a6a'}} className="fa-solid fa-phone"></i>
                     </div>
-                    <div data-aos="fade-left" data-aos-delay='1000' data-aos-duration='1000' className='contact_text_div'>
+                    <div data-aos="fade-left" data-aos-delay='500' data-aos-duration='1000' className='contact_text_div'>
                       <h6 style={{color: '#000000'}}>Mobile No</h6>
                       <a href="tel: 9958949641"><p>9958949641</p></a>
                     </div>
                   </div>
                   <div>
-                    <div data-aos="fade-right" data-aos-delay='1100' data-aos-duration='1000' className='contact_icon_div'>
+                    <div data-aos="fade-right" data-aos-delay='500' data-aos-duration='1000' className='contact_icon_div'>
                       <i style={theme?{color: '#2f4356'}:{color: '#6a6a6a'}} className="fa-solid fa-location-dot"></i>
                     </div>
-                    <div data-aos="fade-left" data-aos-delay='1100' data-aos-duration='1000' className='contact_text_div'>
+                    <div data-aos="fade-left" data-aos-delay='500' data-aos-duration='1000' className='contact_text_div'>
                       <h6 style={{color: '#000000'}}>Address</h6>
                       <a href="https://www.google.com/maps/place/New+Delhi,+Delhi/@28.5273522,77.2089851,11z/data=!3m1!4b1!4m5!3m4!1s0x390cfd5b347eb62d:0x52c2b7494e204dce!8m2!3d28.6139391!4d77.2090212" target={'_blank'}><p>New Delhi, Delhi</p></a>
                     </div>
                   </div>
                   <div>
-                    <div data-aos="fade-right" data-aos-delay='1200' data-aos-duration='1000' className='contact_icon_div'>
+                    <div data-aos="fade-right" data-aos-delay='500' data-aos-duration='1000' className='contact_icon_div'>
                       <i style={theme?{color: '#2f4356'}:{color: '#6a6a6a'}} className="fa-solid fa-envelope"></i>
                     </div>
-                    <div data-aos="fade-left" data-aos-delay='1200' data-aos-duration='1000' className='contact_text_div'>
+                    <div data-aos="fade-left" data-aos-delay='500' data-aos-duration='1000' className='contact_text_div'>
                       <h6 style={{color: '#000000'}}>Email</h6>
                       <a href="mailto: ramzanformasai03@gmail.com"><p>ramzanformasai03@gmail.com</p></a>
                     </div>
                   </div>
                   <div>
-                    <div data-aos="fade-right" data-aos-delay='1300' data-aos-duration='1000' className='contact_icon_div'>
+                    <div data-aos="fade-right" data-aos-delay='500' data-aos-duration='1000' className='contact_icon_div'>
                       <i style={theme?{color: '#2f4356'}:{color: '#6a6a6a'}} className="fa-brands fa-github"></i>
                     </div>
-                    <div data-aos="fade-left" data-aos-delay='1300' data-aos-duration='1000' className='contact_text_div'>
+                    <div data-aos="fade-left" data-aos-delay='500' data-aos-duration='1000' className='contact_text_div'>
                       <h6 style={{color: '#000000'}}>Github</h6>
                       <a href="https://github.com/mr-ramzan01" target={'_blank'}><p>mr-ramzan01</p></a>
                     </div>
                   </div>
                   <div>
-                    <div data-aos="fade-right" data-aos-delay='1400' data-aos-duration='1000' className='contact_icon_div'>
+                    <div data-aos="fade-right" data-aos-delay='500' data-aos-duration='1000' className='contact_icon_div'>
                       <i style={theme?{color: '#2f4356'}:{color: '#6a6a6a'}} className="fa-brands fa-linkedin"></i>
                     </div>
-                    <div data-aos="fade-left" data-aos-delay='1400' data-aos-duration='1000' className='contact_text_div'>
+                    <div data-aos="fade-left" data-aos-delay='500' data-aos-duration='1000' className='contact_text_div'>
                       <h6 style={{color: '#000000'}}>LinkedIn</h6>
                       <a href="https://www.linkedin.com/in/ramzan01/" target={'_blank'}><p>ramzan01</p></a>
                     </div>
@@ -83,15 +83,15 @@ export const Contact = () => {
                 </div>
               </div>
               <form  action="" ref={form} onSubmit={sendEmail} className='contactForm' style={theme?{backgroundColor: '#1e272e'}:{}}>
-                <label data-aos="zoom-in" data-aos-delay='1000' data-aos-duration='1000' htmlFor="">Name<span className='redStar'>*</span></label><br />
-                <input data-aos="fade-left" data-aos-delay='1000' data-aos-duration='1000' style={theme?{backgroundColor: '#1e272e', color: '#fff', border: '1px solid #fff', borderRadius: '3px'}: {}}type="text" required name="from_name"/><br />
-                <label data-aos="zoom-in" data-aos-delay='1100' data-aos-duration='1000' htmlFor="">Email<span className='redStar'>*</span></label><br />
-                <input data-aos="fade-left" data-aos-delay='1100' data-aos-duration='1000' style={theme?{backgroundColor: '#1e272e', color: '#fff', border: '1px solid #fff', borderRadius: '3px'}: {}}type="email" required name="email_id"/><br />
-                <label data-aos="zoom-in" data-aos-delay='1200' data-aos-duration='1000' htmlFor="">Mobile No.<span className='redStar'>*</span></label><br />
-                <input data-aos="fade-left" data-aos-delay='1200' data-aos-duration='1000' style={theme?{backgroundColor: '#1e272e', color: '#fff', border: '1px solid #fff', borderRadius: '3px'}: {}}type="number" required name='mobile_no' minLength={10} maxLength={10}/><br />
-                <label data-aos="zoom-in" data-aos-delay='1300' data-aos-duration='1000' htmlFor="">Message<span className='redStar'>*</span></label><br />
-                <textarea data-aos="fade-left" data-aos-delay='1300' data-aos-duration='1000' style={theme?{backgroundColor: '#1e272e', color: '#fff', border: '1px solid #fff', borderRadius: '3px'}: {}} name="message" id="" cols="15" rows="10" required ></textarea><br />
-                <button style={theme?{background: '#2c3a47'}:{background: '#6a6a6a'}} data-aos="fade-left" data-aos-delay='1400' data-aos-duration='1000' className="submit">Submit</button>
+                <label data-aos="zoom-in" data-aos-delay='500' data-aos-duration='1000' htmlFor="">Name<span className='redStar'>*</span></label><br />
+                <input data-aos="fade-left" data-aos-delay='500' data-aos-duration='1000' style={theme?{backgroundColor: '#1e272e', color: '#fff', border: '1px solid #fff', borderRadius: '3px'}: {}}type="text" required name="from_name"/><br />
+                <label data-aos="zoom-in" data-aos-delay='500' data-aos-duration='1000' htmlFor="">Email<span className='redStar'>*</span></label><br />
+                <input data-aos="fade-left" data-aos-delay='500' data-aos-duration='1000' style={theme?{backgroundColor: '#1e272e', color: '#fff', border: '1px solid #fff', borderRadius: '3px'}: {}}type="email" required name="email_id"/><br />
+                <label data-aos="zoom-in" data-aos-delay='500' data-aos-duration='1000' htmlFor="">Mobile No.<span className='redStar'>*</span></label><br />
+                <input data-aos="fade-left" data-aos-delay='500' data-aos-duration='1000' style={theme?{backgroundColor: '#1e272e', color: '#fff', border: '1px solid #fff', borderRadius: '3px'}: {}}type="number" required name='mobile_no' minLength={10} maxLength={10}/><br />
+                <label data-aos="zoom-in" data-aos-delay='500' data-aos-duration='1000' htmlFor="">Message<span className='redStar'>*</span></label><br />
+                <textarea data-aos="fade-left" data-aos-delay='500' data-aos-duration='1000' style={theme?{backgroundColor: '#1e272e', color: '#fff', border: '1px solid #fff', borderRadius: '3px'}: {}} name="message" id="" cols="15" rows="10" required ></textarea><br />
+                <button style={theme?{background: '#2c3a47'}:{background: '#6a6a6a'}} data-aos="fade-left" data-aos-delay='500' data-aos-duration='1000' className="submit">Submit</button>
               </form>
             </div>
           </div>
--- my_portfolio/src/Components/Navbar.jsx
+++ my_portfolio/src/Components/Navbar.jsx
@@ -1,12 +1,37 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { ThemeContext } from '../Context/ThemeContext'
 import '../styles/Navbar.modules.css'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { Sound } from './Sound';
+import { useRef } from 'react';
 AOS.init();
 
 export const Navbar = () => {
+  const [menu, setMenu] = useState(true);
+  const menuRef = useRef(null);
+  const crossRef = useRef(null);
+  const menuBarRef = useRef(null);
+  const handleMenu = (val) => {
+    // console.log( menuRef.current);
+    // console.log( crossRef.current);
+    if(val) {
+      menuRef.current.style.display = 'none';
+      crossRef.current.style.display = 'flex';
+      menuBarRef.current.style.display = 'block'
+    }
+    else {
+      menuRef.current.style.display = 'flex';
+      crossRef.current.style.display = 'none';
+      menuBarRef.current.style.display = 'none'
+    }
+  }
+
+  const handleMenuBar = () => {
+    menuBarRef.current.style.display = 'none'
+    crossRef.current.style.display = 'none';
+    menuRef.current.style.display = 'flex';
+  }
 
   const {theme, handleTheme} = useContext(ThemeContext);
 
@@ -23,7 +48,7 @@ export const Navbar = () => {
   return (
     <div className='navbar' style={theme? styles.dark: styles.default} >
       <div>
-        <div><div className='logo'><h2>Ramzan Khan</h2><h2 style={theme?{color: '#2c3a47'}: {color: '#0033ff'}}>Ramzan Khan</h2></div></div>
+        <div className='logo_div'><div className='logo'><h2>Ramzan Khan</h2><h2 style={theme?{color: '#2c3a47'}: {color: '#0033ff'}}>Ramzan Khan</h2></div></div>
           <div className='navbox'>
             <div><Sound/></div>
             <h4><a className='link' href="#about" style={theme? styles.dark: styles.default}>About</a></h4>
@@ -46,12 +71,27 @@ export const Navbar = () => {
                 theme ? <span className="material-symbols-outlined" style={{color: "#fff"}}>light_mode</span> : <span className="material-symbols-outlined">dark_mode</span>
               }
             </div>
-            <div className='menu_div'>
+            <div className='menu_div' ref={menuRef} onClick={() => handleMenu(true)}>
               <div style={theme?{backgroundColor: '#fff'}:{backgroundColor: '#1e272e'}}></div>
               <div style={theme?{backgroundColor: '#fff'}:{backgroundColor: '#1e272e'}}></div>
               <div style={theme?{backgroundColor: '#fff'}:{backgroundColor: '#1e272e'}}></div>
             </div>
+            <div className='cross_div' ref={crossRef} onClick={() => handleMenu(false)}>
+              <div style={theme?{backgroundColor: '#fff'}:{backgroundColor: '#1e272e'}}></div>
+              <div style={theme?{backgroundColor: '#fff'}:{backgroundColor: '#1e272e'}}></div>
+            </div>
           </div>
+      </div>
+      <div className='menu_bar' ref={menuBarRef}>
+        <div>
+          <div className="menu_items">
+            <div><h4><a onClick={handleMenuBar} className='link2' href="#about" style={theme? styles.dark: styles.default}>About</a></h4></div>
+            <div><h4><a onClick={handleMenuBar} className='link2' href="#skills" style={theme? styles.dark: styles.default}>Skills</a></h4></div>
+            <div><h4><a onClick={handleMenuBar} className='link2' href="#projects" style={theme? styles.dark: styles.default}>Projects</a></h4></div>
+            <div><h4><a onClick={handleMenuBar} className='link2' href="#stats" style={theme? styles.dark: styles.default}>Github</a></h4></div>
+            <div><h4><a onClick={handleMenuBar} className='link2' href="#contact" style={theme? styles.dark: styles.default}>Contact</a></h4></div>
+          </div>
+        </div>
       </div>
     </div>
     
